1142c7ded3b0e57ff6ab03f96cc5d77399666b59
michaldobiezynski <michaldobiezynski@hotmail.com>
2025-11-22 19:27:47 +0000

    Revert "fix: (moshi-server) add CUDA_COMPUTE_CAP env var to fix NVML build error"
    This reverts commit 4f8330baf6815b2a3b9cb70468962c8b68fb2d29.

--- services/moshi-server/start_moshi_server_private.sh
+++ services/moshi-server/start_moshi_server_private.sh
@@ -4,10 +4,6 @@ set -ex
 
 export LD_LIBRARY_PATH=$(python3 -c 'import sysconfig; print(sysconfig.get_config_var("LIBDIR"))')
 
-# Fix for "Failed to initialize NVML: Unknown Error" during build
-# Default to 80 (Ampere) if not specified
-export CUDA_COMPUTE_CAP=${CUDA_COMPUTE_CAP:-80}
-
 uvx --from 'huggingface_hub[cli]' huggingface-cli login --token $HUGGING_FACE_HUB_TOKEN
 
-CUDA_COMPUTE_CAP=${CUDA_COMPUTE_CAP} cargo run --features=cuda --bin=moshi-server -r -- $@
+cargo run --features=cuda --bin=moshi-server -r -- $@
--- services/moshi-server/start_moshi_server_public.sh
+++ services/moshi-server/start_moshi_server_public.sh
@@ -4,13 +4,9 @@ set -ex
 
 export LD_LIBRARY_PATH=$(python3 -c 'import sysconfig; print(sysconfig.get_config_var("LIBDIR"))')
 
-# Fix for "Failed to initialize NVML: Unknown Error" during build
-# Default to 80 (Ampere) if not specified
-export CUDA_COMPUTE_CAP=${CUDA_COMPUTE_CAP:-80}
-
 uvx --from 'huggingface_hub[cli]' huggingface-cli login --token $HUGGING_FACE_HUB_TOKEN
 
-CARGO_TARGET_DIR=/app/target CUDA_COMPUTE_CAP=${CUDA_COMPUTE_CAP} cargo install --features cuda moshi-server@0.6.3
+CARGO_TARGET_DIR=/app/target cargo install --features cuda moshi-server@0.6.3
 
 # Subtle detail here: We use the full path to `moshi-server` because there is a `moshi-server` binary
 # from the `moshi` Python package. We'll fix this conflict soon.
