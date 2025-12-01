cfa1dd1965d6bedc45dcfbcbdd09c9e6a8285764
michaldobiezynski <michaldobiezynski@hotmail.com>
2025-12-01 14:41:55 +0000

    feat: (vllm) increase max-model-len to 16384 and gpu-memory-utilization to 0.5

--- dockerless/start_llm.sh
+++ dockerless/start_llm.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")/.."
 
 uv tool run vllm@v0.9.1 serve \
   --model=google/gemma-3-1b-it \
-  --max-model-len=8192 \
+  --max-model-len=16384 \
   --dtype=bfloat16 \
-  --gpu-memory-utilization=0.3 \
+  --gpu-memory-utilization=0.5 \
   --port=8091
